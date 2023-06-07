2208c41deec6b69e606420e66f85ba9895a5ee39
Qasimsaifi <106295428+Qasimsaifi@users.noreply.github.com>
2023-06-07 16:53:44 +0530

    dfgfg

--- blogapi/settings.py
+++ blogapi/settings.py
@@ -150,11 +150,13 @@ CSRF_TRUSTED_ORIGINS = [
 #     # Add other trusted frontend domains as needed
 # ]
 STATIC_URL = '/static/'
-STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfile')
-# # Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'static'),
-)
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
