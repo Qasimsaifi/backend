e2703c105a503c9084d09e34a2dbafc653fcfc5d
Qasimsaifi <106295428+Qasimsaifi@users.noreply.github.com>
2023-06-07 17:30:24 +0530

    fgg

--- blogapi/urls.py
+++ blogapi/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from django.urls import path
-from django.urls import path , include
+from django.urls import path, include
+from django.http import HttpResponse
 
+def welcome(request):
+    return HttpResponse("Welcome to BlogAxis API")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('blogpost.urls')),
+    path('', welcome),
+    path('api/v1/', include('blogpost.urls')),
 ]
--- blogpost/urls.py
+++ blogpost/urls.py
@@ -7,6 +7,6 @@ router.register('posts', views.BlogPostViewSet)
 
 urlpatterns = [
     # ...
-    path('', include(router.urls)),
+    path('/', include(router.urls)),
     path('auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
