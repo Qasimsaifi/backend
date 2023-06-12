6af31e9c8b2d1e50a8b4b539b7bdc2379e28b6ac
Qasimsaifi <106295428+Qasimsaifi@users.noreply.github.com>
2023-06-12 15:06:28 +0530

    changed

--- snippets/models.py
+++ snippets/models.py
@@ -15,7 +15,7 @@ class CodeSnippet(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField(unique=True, max_length=255, default='', null=True)
     content = HTMLField()
-    code_snippet = models.TextField(default=None, null=True)
+    code_snippet = HTMLField()
     image = CloudinaryField('image', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -23,19 +23,6 @@ class CodeSnippet(models.Model):
 
     def __str__(self):
         return self.slug
-
-    def save(self, *args, **kwargs):
-        # Generate slug from the title if it's not set
-        if not self.slug:
-            self.slug = slugify(self.title)
-
-        # Escape the code snippet content to prevent HTML injection
-        escaped_code = escape(self.code_snippet)
-        # Wrap the escaped code in <pre> tags
-        formatted_code = f"<pre>{escaped_code}</pre>"
-        self.code_snippet = formatted_code
-
-        super().save(*args, **kwargs)
 
 
 class Comment(models.Model):
