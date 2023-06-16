cb5c05c9915df03316d5d9259f030f0af606858f
Qasimsaifi <106295428+Qasimsaifi@users.noreply.github.com>
2023-06-16 16:44:52 +0530

    add is private

--- snippets/admin.py
+++ snippets/admin.py
@@ -10,4 +10,4 @@ class CodeSnippetAdmin(admin.ModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ["id","snippet", "content" , "author"]
+    list_display = ["id","snippet", "content" , "author", "is_private"]
