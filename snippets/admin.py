948f174380c31d8270a9fa06441670b1a93152e5
Qasimsaifi <106295428+Qasimsaifi@users.noreply.github.com>
2023-06-16 16:41:30 +0530

    add author field to show in admin panel

--- snippets/admin.py
+++ snippets/admin.py
@@ -10,4 +10,4 @@ class CodeSnippetAdmin(admin.ModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ["id","snippet", "content"]
+    list_display = ["id","snippet", "content" , "author"]
