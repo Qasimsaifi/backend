6e8556133a73390e052896babc2e5c5e8f738549
Qasimsaifi <106295428+Qasimsaifi@users.noreply.github.com>
2023-06-21 13:13:48 +0530

    cahnged

--- snippets/models.py
+++ snippets/models.py
@@ -3,7 +3,7 @@ from cloudinary.models import CloudinaryField
 from tinymce.models import HTMLField
 from django.contrib.auth.models import UserManager
 from django.conf import settings
-
+import datetime
 
 class CodeSnippet(models.Model):
     PRIVACY_CHOICES = (
@@ -31,12 +31,14 @@ class Comment(models.Model):
     snippet = models.ForeignKey(CodeSnippet, on_delete=models.CASCADE)
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        default=None,  # Replace 1 with the ID of the default author or an appropriate value
+        default=None,
         on_delete=models.CASCADE,
-        
     )
-
+    author_name = models.CharField(max_length=255, null=True, default=None)
+    author_email = models.EmailField(null=True, default=None)
+    author_picture = models.URLField(null=True, default=None)
     content = models.TextField()
+    created_at = models.DateTimeField(default=datetime.datetime.now)
 
     def __str__(self):
         return self.content
