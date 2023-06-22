f6418f98c29dc1f3ea0713a137183556fa1bd6d0
Qasimsaifi <106295428+Qasimsaifi@users.noreply.github.com>
2023-06-23 03:13:47 +0530

    remove

--- snippets/models.py
+++ snippets/models.py
@@ -21,7 +21,7 @@ class CodeSnippet(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField(unique=True, max_length=255, default="", null=True)
     content = HTMLField()
-    code_snippet = HTMLField(null=True)
+    code_snippet = models.CharField(null=True)
     language = models.CharField(choices=LANGUAGES_CHOICES,  max_length=100 , default="javascipt")
     image = CloudinaryField("image", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
