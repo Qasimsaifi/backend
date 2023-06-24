25a27ffd09fbfe2f6c066fc3bcf34fcdbf47abc7
Qasimsaifi <106295428+Qasimsaifi@users.noreply.github.com>
2023-06-25 02:02:57 +0530

    yahu

--- accounts/models.py
+++ accounts/models.py
@@ -1,22 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from cloudinary.models import CloudinaryField
-
+import uuid
 from .manager import UserManager
 
+
+def generate_unique_username():
+    return str(uuid.uuid4())
+
+
 class User(AbstractUser):
-    username = None
+    username = models.CharField(
+        max_length=24,
+        default=generate_unique_username,
+        unique=True,
+    )
     email = models.EmailField(unique=True)
     mobile = models.CharField(max_length=14)
-    profile_picture = CloudinaryField('image', blank=True, null=True)
+    profile_picture = CloudinaryField("image", blank=True, null=True)
     email_token = models.CharField(max_length=100, null=True, blank=True)
     forget_password = models.CharField(max_length=100, null=True, blank=True)
     last_login_time = models.DateTimeField(null=True, blank=True)
     last_logout_time = models.DateTimeField(null=True, blank=True)
+
     def __str__(self):
         return self.email
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
